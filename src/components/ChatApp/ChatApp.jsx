1b7765c48990f4b4e49bada909e8c3db6a75713a
hamongkhang <hamongkhang@gmail.com>
2023-06-06 05:34:41 +0700

    feat: add new images

--- src/components/ChatApp/ChatApp.jsx
+++ src/components/ChatApp/ChatApp.jsx
@@ -1,4 +1,4 @@
-import { Grid, Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import Menu from "../Menu/Menu";
 import ChatContent from "../ChatContent/ChatContent";
 import React from "react";
@@ -8,7 +8,7 @@ function ChatApp() {
   return (
     <Grid container columns={{ xs: 4, sm: 8, md: 12 }} className="app_container">
     <Grid item xs={4} sm={8} md={2} className="left_app">
-         Phần bên trái 
+        <Menu />
     </Grid>
     <Grid item xs={4} sm={8} md={10} className="right_app">
         Phần bên phải
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,14 +1,61 @@
 import React from "react";
 import { Box, Grid, Typography } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
 
 const Menu = () => {
-  const theme = useTheme();
-
   return (
-    <Box
-      className="menu_container"
-    ></Box>
+    <Box className="menu">
+      <Grid container columns={{ xs: 4, sm: 8, md: 12 }}>
+        <Grid item xs={4} sm={8} md={12} className="menu_item1">
+          <div
+            style={{
+              paddingTop: 16,
+              paddingRight: 28,
+              paddingBottom: 16,
+              paddingLeft: 28,
+              backgroundColor: "#1F1F1F",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <div class="image-container">
+              <img
+                onClick={() => window.location.reload()}
+                src={process.env.REACT_APP_IMAGE + "/images/icon_main.png"}
+                alt="Ảnh"
+              />
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={4} sm={8} md={12} className="menu_item2">
+          <div
+            style={{
+              paddingTop: 16,
+              paddingRight: 28,
+              paddingBottom: 16,
+              paddingLeft: 28,
+              backgroundColor: "#383838",
+              height: 76,
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <img
+              src={
+                process.env.REACT_APP_IMAGE + "/images/chatgpt-icon-logo.png"
+              }
+              alt="Ảnh"
+              style={{
+                width: "44px",
+                height: "44px",
+              }}
+            />
+            <Typography sx={{ marginLeft: 1, fontSize: "12px" }}>
+              ChatGPT
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
