b2d08449d7feb237e7eb68b8d01d95049b657c48
hamongkhang <hamongkhang@gmail.com>
2023-06-07 12:22:44 +0700

    feat: responsive for content chat

--- src/components/ChatContent/ChatContent.jsx
+++ src/components/ChatContent/ChatContent.jsx
@@ -6,7 +6,30 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import axios from "axios";
 
 const ChatContent = () => {
-  return <Box className="Content_Container">haha</Box>;
+  const chatContainerRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [listMessage, setListMessage] = useState([]);
+  const [suggest, setSuggest] = useState();
+  const [isDropdown, setIsDropdown] = useState(false);
+  const [isError, setIsError] = useState(false);
+  return (
+    <Box className="content_container">
+      <Box className="content_container_top">
+        {listMessage.length ? null : (
+          <Box className="background_image">
+            <img
+              src="https://hamongkhang.github.io/CHATGPT_APP/images/content.png"
+              alt="Ảnh"
+            />
+          </Box>
+        )}
+      </Box>
+
+      <Box className="content_container_bottom">ádad</Box>
+    </Box>
+  );
 };
 
 export default ChatContent;
